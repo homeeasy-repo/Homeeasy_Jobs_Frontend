2c6f1e6861e8086400bee20f9fa0ec1db0909bad
ZainXaidi <zain@homeeasy.com>
2024-06-02 03:34:45 +0500

    commit

--- api.py
+++ api.py
@@ -3,33 +3,63 @@ import traceback
 
 app = Flask(__name__)
 
-@app.route("/hiring/resumes", methods=["GET", "POST"])
+@app.route("/https://jobs.homeeasy.com/hiring/resumesubmission", methods=["GET", "POST"])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('https://jobs.homeeasy.com/')
     elif request.method == 'POST':
         try:
-            if 'resume' not in request.files:
-                return jsonify({"status": "error", "error": "No file part"}), 400
-            
-            resume = request.files['resume']
-            if resume.filename == '':
-                return jsonify({"status": "error", "error": "No selected file"}), 400
+            zapier_data = request.get_json(force=True)
+            new_candidates = []
 
-            # Save the file or process it here
-            # For example: resume.save(os.path.join("uploads", secure_filename(resume.filename)))
-            
-            # Extract other form data
-            first_name = request.form['first-name']
-            last_name = request.form['last-name']
-            email = request.form['email']
-            phone = request.form['phone']
-            linkedin = request.form['linkedin']
-            city = request.form['city']
-            print(first_name,last_name,email,phone,linkedin,city)
-            
-            # Process the data as needed
-            return jsonify({"status": "success"}), 200
+            if isinstance(zapier_data, dict):  
+                first_name = request.form['first-name']
+                last_name = request.form['last-name']
+                email = request.form['email']
+                phone = request.form['phone']
+                linkedin = request.form['linkedin']
+                city = request.form['city']
+                emailed_on = datetime.now()
+                        
+                # Hash email to generate indeed_id
+                indeed_id = hashlib.sha256(email.encode()).hexdigest()
+
+                # Check if the candidate already exists in the database
+                existing_candidate = IndeedCandidate.query.filter_by(indeed_id=indeed_id).first()
+
+                if not existing_candidate:
+                    
+                    try:
+                        fname = first_name.upper()[0] + first_name[1:].lower()
+                    except IndexError:
+                        pass
+                    
+                    try:
+                        lname = last_name.upper()[0] + last_name[1:].lower()
+                    except IndexError:
+                        pass
+                    
+                    new_candidate = {
+                        'indeed_id': indeed_id,
+                        'indeed_email': email,
+                        'fname': fname,
+                        'lname': lname,
+                        'city': city,
+                        'alternate_email': None,
+                        'phone': phone,
+                        'hiring_status_id': 1,
+                        'linkedin' : linkedin,
+                        'emailed_on': emailed_on,
+                    }
+                    
+                    new_candidates.append(new_candidate)
+                # Perform bulk insert
+                # if new_candidates:
+                #     tasks.async_sendActivityEmails.delay(new_candidates, in_person=False)
+                #     db.session.bulk_insert_mappings(IndeedCandidate, new_candidates)
+                #     db.session.commit()
+
+                return jsonify({"status": "success"}), 200
         except Exception as e:
             print(f'Error: {e}, Trace: {traceback.format_exc()}')
             return jsonify({"status": "error", "error": str(e)}), 400
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if new_candidates:
+                #     tasks.async_sendActivityEmails.delay(new_candidates, in_person=False)
+                #     db.session.bulk_insert_mappings(IndeedCandidate, new_candidates)
+                #     db.session.commit()
