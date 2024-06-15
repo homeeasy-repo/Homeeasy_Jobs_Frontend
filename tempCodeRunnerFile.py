9f71a49a829952005e6639556950834ea8d43d9c
ZainXaidi <zain@homeeasy.com>
2024-06-15 21:47:36 +0500

    Changed frontend

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-if new_candidates:
-                #     tasks.async_sendActivityEmails.delay(new_candidates, in_person=False)
-                #     db.session.bulk_insert_mappings(IndeedCandidate, new_candidates)
-                #     db.session.commit()
